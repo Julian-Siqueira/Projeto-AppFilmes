f168aae0e3dd089c7f452f72b192e7e32edf86fe
Julian-Siqueira <juliansiqueiradev@gmail.com>
2025-01-20 14:15:30 -0300

    Validando Lista de Filmes

---
+++ app/src/main/java/com/example/appfilmes/Adapter/AdapterFilme.java
@@ -0,0 +1,61 @@
+package com.example.appfilmes.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.appfilmes.Model.Filme;
+import com.example.appfilmes.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterFilme extends RecyclerView.Adapter<AdapterFilme.FilmeViewHolder> {
+
+    private Context context;
+    private List<Filme> filmeList = new ArrayList<>();
+
+    public AdapterFilme(Context context, List<Filme> filmeList) {
+        this.context = context;
+        this.filmeList = filmeList;
+    }
+
+    @NonNull
+    @Override
+    public FilmeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemList;
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        itemList = layoutInflater.inflate(R.layout.filme_item, parent, false);
+        return new FilmeViewHolder(itemList);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FilmeViewHolder holder, int position) {
+        holder.capaFilme.setImageResource(filmeList.get(position).getCapa());
+        holder.titulo.setText(filmeList.get(position).getTitulo());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return filmeList.size();
+    }
+
+    public class FilmeViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView capaFilme;
+        private TextView titulo;
+
+        public FilmeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            capaFilme = itemView.findViewById(R.id.capa_filme);
+            titulo = itemView.findViewById(R.id.titulo_filme);
+        }
+    }
+}
--- app/src/main/java/com/example/appfilmes/MainActivity.java
+++ app/src/main/java/com/example/appfilmes/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.appfilmes;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.appfilmes.Adapter.AdapterFilme;
+import com.example.appfilmes.Model.Filme;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerViewFilmes;
+    private List<Filme> listFilmes;
+    private AdapterFilme adapterFilme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         inicializarComponentes();
+
+        listFilmes = new ArrayList<>();
+        adapterFilme = new AdapterFilme(getApplicationContext(), listFilmes);
+        recyclerViewFilmes.setAdapter(adapterFilme);
+        recyclerViewFilmes.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+        recyclerViewFilmes.setHasFixedSize(true);
+        filmes();
+
+
+
+    }
+
+    private void filmes() {
+
+        Filme filme1 = new Filme(R.drawable.ic_launcher_foreground, "filme 1");
+        listFilmes.add(filme1);
+        Filme filme2 = new Filme(R.drawable.ic_launcher_background, "filme 2");
+        listFilmes.add(filme2);
+        Filme filme3 = new Filme(R.drawable.ic_launcher_foreground, "filme 3");
+        listFilmes.add(filme3);
+        Filme filme4 = new Filme(R.drawable.ic_launcher_background, "filme 4");
+        listFilmes.add(filme4);
+        Filme filme5 = new Filme(R.drawable.ic_launcher_foreground, "filme 5");
+        listFilmes.add(filme5);
+        Filme filme6 = new Filme(R.drawable.ic_launcher_background, "filme 6");
+        listFilmes.add(filme6);
+
     }
 
     private void inicializarComponentes() {
